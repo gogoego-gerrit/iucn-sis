eaab4a2a9983c79a5e5738550c26a379128df4da
carl.scott <carl.scott@solertium.com>
2010-08-03 16:57:32 -0400

    Test SIS via runnable bootstrap
    This project requires an up-to-date com.solertium.gogoego.server.extensions.testing, but this project does not and should not be pushed out to any server.
    
    Change-Id: I61dc7c51cf5855e8335338836cf8c62e5c0f4e54
    Reviewed-on: http://review.source.gogoego.com/375
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

---
+++ org.iucn.sis.server.testing/src/SISTestBootstrap.java
@@ -0,0 +1,48 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.gogoego.api.applications.GoGoEgoApplication;
+import org.iucn.sis.server.ServerActivator;
+
+import com.solertium.gogoego.server.extensions.testing.generic.GenericBootstrap;
+import com.solertium.gogoego.server.extensions.testing.generic.MultiAppGenericBootstrap;
+
+/**
+ * Use this to run the entire SIS universe.  If you 
+ * just want to run a particular application, make 
+ * a new bootstrap that extends {@link GenericBootstrap} 
+ * 
+ * 
+ * @author carl.scott@solertium.com
+ *
+ */
+public class SISTestBootstrap extends MultiAppGenericBootstrap {
+	
+	public static void main(String[] args) {
+		final SISTestBootstrap b;
+		try {
+			b = new SISTestBootstrap();
+		} catch (Exception e) {
+			System.out.println("Could not create test bootstrap");
+			throw new RuntimeException(e);
+		}
+		try {
+			b.start();
+		} catch (Exception e) {
+			System.out.println("Could not start test bootstrap");
+			throw new RuntimeException(e);
+		}
+	}
+	
+	protected Map<String, GoGoEgoApplication> getGoGoEgoApplications() {
+		final Map<String, GoGoEgoApplication> map = 
+			new LinkedHashMap<String, GoGoEgoApplication>();
+		
+		map.put("org.iucn.sis.server", new ServerActivator().getApplicationFactory().newInstance());
+		//TODO add additional plugins
+		
+		
+		return map;
+	}
+
+}
