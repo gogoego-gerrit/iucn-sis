7a4bc835fcb1deb75e00c799e0df96cbbad87e18
carl.scott <carl.scott@solertium.com>
2010-09-24 16:06:52 -0400

    List all fields available in the system.
    Peruses the lookup database for all the 
    fields, making them available for extensions.
    
    Change-Id: I0235f788cc8e5c48ebb4e434925e4f6b59133d9d
    Reviewed-on: http://review.source.gogoego.com/860
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.server.api/src/org/iucn/sis/server/api/fields/definitions/FieldDefinitionLoader.java
+++ org.iucn.sis.server.api/src/org/iucn/sis/server/api/fields/definitions/FieldDefinitionLoader.java
@@ -1,10 +1,48 @@
 package org.iucn.sis.server.api.fields.definitions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import org.iucn.sis.server.api.application.SIS;
 import org.w3c.dom.Document;
 
+import com.solertium.db.DBException;
+import com.solertium.db.ExecutionContext;
+import com.solertium.db.SystemExecutionContext;
 import com.solertium.util.BaseDocumentUtils;
 
 public class FieldDefinitionLoader {
+	
+	private static List<String> allFields = null;
+	
+	public static List<String> getAllFields() {
+		if (allFields != null)
+			return allFields;
+		
+		allFields = new ArrayList<String>();
+		
+		ExecutionContext ec = SIS.get().getLookupDatabase();
+		
+		List<String> tables;
+		try  {
+			tables = ec.getDBSession().listTables(ec);
+		} catch (DBException e) {
+			e.printStackTrace();
+			return allFields;
+		}
+		
+		for (String table : tables) {
+			String tableLower = table.toLowerCase();
+			if (table.contains("_") || tableLower.endsWith("lookup") || tableLower.endsWith("subfield"))
+				continue;
+			
+			allFields.add(table);
+		}
+		
+		return allFields;
+	}
 	
 	public static Document get(String fieldName) {
 		String file = fieldName;
--- org.iucn.sis.server.api/src/org/iucn/sis/server/api/io/FieldIO.java
+++ org.iucn.sis.server.api/src/org/iucn/sis/server/api/io/FieldIO.java
@@ -1,10 +1,30 @@
 package org.iucn.sis.server.api.io;
 
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import org.iucn.sis.server.api.fields.FieldSchemaGenerator;
+import org.iucn.sis.server.api.fields.definitions.FieldDefinitionLoader;
 import org.iucn.sis.server.api.persistance.FieldDAO;
 import org.iucn.sis.server.api.persistance.hibernate.PersistentException;
 import org.iucn.sis.shared.api.models.Field;
+import org.w3c.dom.Document;
 
 public class FieldIO {
+	
+	private final FieldSchemaGenerator generator;
+	
+	public FieldIO() {
+		FieldSchemaGenerator generator = null;
+		try {
+			generator = new FieldSchemaGenerator();
+		} catch (NamingException e) {
+			System.err.println("sis_lookups database is not defined.");
+		}
+		
+		this.generator = generator;
+	}
 
 	public Field get(Integer fieldID) {
 		try {
@@ -14,6 +34,18 @@ public class FieldIO {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	public List<String> getAllFields() {
+		return FieldDefinitionLoader.getAllFields();
+	}
+	
+	public Field getDefinition(String fieldName) throws Exception {
+		return generator.getField(fieldName);
+	}
+	
+	public Document getSchema(String fieldName) throws Exception {
+		return generator.getSchema(fieldName);
 	}
 	
 }
