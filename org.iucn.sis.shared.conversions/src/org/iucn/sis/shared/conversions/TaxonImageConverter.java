5675aa6ac2e17651a477b4542029461e2d623895
Carl Scott <carl.scott@solertium.com>
2011-03-22 18:00:45 -0400

    Fixed a logic error in the TaxonImageConverter
    Was not recursing into folders properly, and paths
    were wrong for top-level files.
    
    Change-Id: Ib70fa96f56beaac7b2f2009e64404fc1103e27b5
    Reviewed-on: http://review.source.gogoego.com/2228
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.shared.conversions/src/org/iucn/sis/shared/conversions/TaxonImageConverter.java
+++ org.iucn.sis.shared.conversions/src/org/iucn/sis/shared/conversions/TaxonImageConverter.java
@@ -14,6 +14,7 @@ import org.iucn.sis.shared.api.models.TaxonImage;
 import com.solertium.util.BaseTagListener;
 import com.solertium.util.TagFilter;
 import com.solertium.util.TagFilter.Tag;
+import com.solertium.vfs.NotFoundException;
 import com.solertium.vfs.VFSPath;
 import com.solertium.vfs.VFSPathToken;
 
@@ -49,69 +50,75 @@ public class TaxonImageConverter extends GenericConverter<VFSInfo> {
 	private void copyXML(VFSPath path, final AtomicInteger batchSize) throws Exception {
 		batchIfNeeded(batchSize);
 		
-		for (VFSPathToken token : path.getTokens()) {
-			final VFSPath uri = path.child(token);
-			if (data.getOldVFS().isCollection(uri)) {
-				copyXML(path, batchSize);
-			}
-			else if (token.toString().endsWith(".xml")) {
-				try {
-					int index = token.toString().lastIndexOf('.');
-					Integer taxonID = Integer.valueOf(token.toString().substring(0, index));
-					
-					final TaxonIO io = new TaxonIO(session);
-					final Taxon taxon = io.getTaxon(taxonID);
-					if (taxon == null) {
-						printf("No taxon saved with the ID %s, skipping...", taxonID);
-						continue;
-					}
-					
-					final TagFilter tf = new TagFilter(data.getOldVFS().getReader(uri));
-					tf.shortCircuitClosingTags = true;
-					tf.registerListener(new BaseTagListener() {
-						public void process(Tag t) throws IOException {
-							TaxonImage image = new TaxonImage();
-							image.setCaption(t.getAttribute("caption"));
-							image.setCredit(t.getAttribute("credit"));
-							image.setEncoding(t.getAttribute("encoding"));
-							image.setIdentifier(t.getAttribute("id"));
-							image.setPrimary("true".equalsIgnoreCase(t.getAttribute("primary")));
-							try {
-								image.setRating(Float.valueOf(t.getAttribute("rating")));
-							} catch (Exception e) {
-								image.setRating(0.0F);
-							}
-							image.setShowRedList("true".equalsIgnoreCase(t.getAttribute("showRedlist")));
-							image.setShowSIS("true".equalsIgnoreCase(t.getAttribute("showSIS")));
-							image.setSource(t.getAttribute("source"));
-							image.setTaxon(taxon);
-							try {
-								image.setWeight(Integer.valueOf(t.getAttribute("weight")));
-							} catch (Exception e) {
-								image.setWeight(0);
-							}
+		if (data.getOldVFS().isCollection(path)) {
+			VFSPathToken[] tokens = data.getOldVFS().list(path);
+			for (VFSPathToken token : tokens)
+				copyXML(path.child(token), batchSize);
+		}
+		else if (path.getName().endsWith(".xml"))
+			convert(path, batchSize);
+	}
 	
-							session.save(image);
-							batchIfNeeded(batchSize);
-						}
-						public List<String> interestingTagNames() {
-							List<String> l = new ArrayList<String>();
-							l.add("image");
-							return l;
-						}
-					});
-					tf.parse();
-				} catch (Exception e) {
-					printf("Skipping images for {0} due to exception: {1}", token, e.getMessage());
-					continue;
-				}
+	private void convert(VFSPath uri, final AtomicInteger batchSize) throws Exception {		
+		try {
+			int index = uri.getName().lastIndexOf('.');
+			Integer taxonID = Integer.valueOf(uri.getName().substring(0, index));
+			
+			final TaxonIO io = new TaxonIO(session);
+			final Taxon taxon = io.getTaxon(taxonID);
+			if (taxon == null) {
+				printf("No taxon saved with the ID %s, skipping...", taxonID);
+				return;
 			}
+			
+			final TagFilter tf = new TagFilter(data.getOldVFS().getReader(uri));
+			tf.shortCircuitClosingTags = true;
+			tf.registerListener(new BaseTagListener() {
+				public void process(Tag t) throws IOException {
+					TaxonImage image = new TaxonImage();
+					image.setCaption(t.getAttribute("caption"));
+					image.setCredit(t.getAttribute("credit"));
+					image.setEncoding(t.getAttribute("encoding"));
+					image.setIdentifier(t.getAttribute("id"));
+					image.setPrimary("true".equalsIgnoreCase(t.getAttribute("primary")));
+					try {
+						image.setRating(Float.valueOf(t.getAttribute("rating")));
+					} catch (Exception e) {
+						image.setRating(0.0F);
+					}
+					image.setShowRedList("true".equalsIgnoreCase(t.getAttribute("showRedlist")));
+					image.setShowSIS("true".equalsIgnoreCase(t.getAttribute("showSIS")));
+					image.setSource(t.getAttribute("source"));
+					image.setTaxon(taxon);
+					try {
+						image.setWeight(Integer.valueOf(t.getAttribute("weight")));
+					} catch (Exception e) {
+						image.setWeight(0);
+					}
+
+					session.save(image);
+					batchIfNeeded(batchSize);
+				}
+				public List<String> interestingTagNames() {
+					List<String> l = new ArrayList<String>();
+					l.add("image");
+					return l;
+				}
+			});
+			tf.parse();
+		} catch (NotFoundException e) {
+			printf("Could not find %s", uri);
+		} catch (Exception e) {
+			printf("Skipping images for %s due to exception: %s", uri, e.getMessage());
+			e.printStackTrace();
 		}
 	}
 	
 	private void batchIfNeeded(AtomicInteger batchSize) {
-		if (batchSize.incrementAndGet() >= 25)
+		if (batchSize.incrementAndGet() >= 25) {
+			printf("Created %s images...", batchSize.get());
 			commitAndStartTransaction();
+		}
 	}
 	
 	private void copyImages(VFSPath path) throws Exception {
