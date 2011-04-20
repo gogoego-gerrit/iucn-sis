04d2a012ec43ee34b42db8d9985b65dc0f4f8191
Carl Scott <carl.scott@solertium.com>
2011-04-20 16:57:50 -0400

    NotesWindow Improvements.
    * Sized the text to make it easier to read.
    * Replace carriage returns with br tags.
    * Added horizontal rule between note entries.
    * Added additional vertical spacing between entries.
    * Notes list scrolls when necessary.
    * Added comparator to keep notes in order, newest first.
    * Sized the window to be taller.
    
    These changes apply to Taxon notes, and any other SIS
    component that uses notes.
    
    See #396
    
    Change-Id: Ib04f021b8f8e37428434c6fc1df5ecb72e8092f2
    Reviewed-on: http://review.source.gogoego.com/2341
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.client.shared/src/org/iucn/sis/client/api/ui/notes/NotesWindow.java
+++ org.iucn.sis.client.shared/src/org/iucn/sis/client/api/ui/notes/NotesWindow.java
@@ -1,7 +1,11 @@
 package org.iucn.sis.client.api.ui.notes;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 import org.iucn.sis.client.api.container.SISClientBase;
 import org.iucn.sis.client.api.utils.FormattedDate;
@@ -10,24 +14,26 @@ import org.iucn.sis.shared.api.models.Edit;
 import org.iucn.sis.shared.api.models.Notes;
 
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
-import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.Style.Scroll;
+import com.extjs.gxt.ui.client.Style.VerticalAlignment;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.event.WindowEvent;
+import com.extjs.gxt.ui.client.widget.HorizontalPanel;
 import com.extjs.gxt.ui.client.widget.HtmlContainer;
 import com.extjs.gxt.ui.client.widget.Info;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.VerticalPanel;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.TextArea;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.FillLayout;
-import com.extjs.gxt.ui.client.widget.layout.RowData;
-import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.extjs.gxt.ui.client.widget.layout.TableData;
+import com.extjs.gxt.ui.client.widget.layout.TableLayout;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Image;
@@ -45,7 +51,7 @@ public class NotesWindow extends Window implements DrawsLazily {
 		this.api = api;
 		
 		setLayout(new FillLayout());
-		setSize(500, 400);
+		setSize(500, 550);
 		setHeading("View Notes");
 	}
 	
@@ -73,12 +79,18 @@ public class NotesWindow extends Window implements DrawsLazily {
 					container.add(new HtmlContainer("<div style='padding-top:10px';background-color:grey>"
 							+ "<b>There are no notes for this field.</b></div>"), top);
 				} else {
-					LayoutContainer eBar = new LayoutContainer(new FillLayout(Orientation.VERTICAL));
-					eBar.setHeight(200);
-					eBar.setLayoutOnChange(true);
+					TableLayout layout = new TableLayout(1);
+					layout.setCellPadding(0);
+					layout.setCellSpacing(0);
+					
+					TableData layoutData = new TableData();
+					layoutData.setMargin(0);
+					layoutData.setPadding(0);
+					
+					LayoutContainer eBar = new LayoutContainer(layout);
 					eBar.setScrollMode(Scroll.AUTO);
 
-					for (final Notes current : notes) {
+					for (final Notes current : sorted(notes)) {
 						Image deleteNote = new Image("images/icon-note-delete.png");
 						deleteNote.setTitle("Delete Note");
 						deleteNote.addClickHandler(new ClickHandler() {
@@ -95,12 +107,21 @@ public class NotesWindow extends Window implements DrawsLazily {
 							}
 						});
 
-						LayoutContainer a = new LayoutContainer(new RowLayout(Orientation.HORIZONTAL));
-						a.add(deleteNote, new RowData());
-						a.add(new HtmlContainer(createEditLabelText(current) + " -- " + current.getValue()), new RowData(1d, 1d));
+						HorizontalPanel a = new HorizontalPanel();
+						a.addStyleName("notes_entry");
+						a.setVerticalAlign(VerticalAlignment.TOP);
+						a.setSpacing(4);
+						a.add(deleteNote);
+						a.add(new HtmlContainer("<div class=\"notes_entry_html\">" + 
+							createEditLabelText(current) + " -- " + toHTML(current.getValue()) + "</div>"));
 
-						eBar.add(a, new RowData(1d, 1d));
+						eBar.add(a, layoutData);
 					}
+					
+					LayoutContainer wrapper = new LayoutContainer(new FillLayout());
+					wrapper.setScrollMode(Scroll.AUTOX);
+					wrapper.add(eBar);
+					
 					container.add(eBar, top);
 				}
 
@@ -154,6 +175,13 @@ public class NotesWindow extends Window implements DrawsLazily {
 		});
 	}
 	
+	private List<Notes> sorted(Collection<Notes> notes) {
+		final List<Notes> sorted = new ArrayList<Notes>(notes);
+		Collections.sort(sorted, new NotesComparator());
+		
+		return sorted;
+	}
+	
 	protected String createEditLabelText(Notes note) {
 		final String defaultValue = "<i>Edit information unavailable</i>";
 		if (note.getEdits() == null || note.getEdits().isEmpty())
@@ -168,6 +196,34 @@ public class NotesWindow extends Window implements DrawsLazily {
 			Debug.println(e);
 			return defaultValue;
 		}
+	}
+	
+	protected String toHTML(String value) {
+		return value.replace("\n", "<br/>");
+	}
+	
+	private static class NotesComparator implements Comparator<Notes> {
+		
+		@Override
+		public int compare(Notes o1, Notes o2) {
+			Edit e1 = o1.getEdit();
+			Edit e2 = o2.getEdit();
+			
+			if (e1 == null && e2 == null)
+				return 0;
+			else if (e1 == null)
+				return 1;
+			else if (e2 == null)
+				return -1;
+			
+			int result = e1.getCreatedDate().compareTo(e2.getCreatedDate());
+			
+			if (result == 0)
+				result = Integer.valueOf(o1.getId()).compareTo(Integer.valueOf(o2.getId()));
+			
+			return result * -1;
+		}
+		
 	}
 
 }
