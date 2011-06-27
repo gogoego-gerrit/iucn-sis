80e32b63b258cee3c0c2b680936073c731e165fe
carl.scott <carl.scott@solertium.com>
2011-06-27 14:25:49 -0400

    Added the bad C2b assessment to ExpertSystem tests
    As described in the ticket, includes only the population
    size of 230 and extreme fluc for mature individuals set
    to Yes.  This alone should not trigger C2b as C2 is not
    yet met, but it does.
    
    Also, added a good version that does include the C2 point
    to ensure that it DOES come out as C2b when there is a
    continuing decline.
    
    See #437
    
    Change-Id: I57282e230f8d3bb3384cea601ff1dd5bdcb51660
    Reviewed-on: http://review.source.gogoego.com/2528
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.server.testing/test/core/ExpertSystem.java
+++ org.iucn.sis.server.testing/test/core/ExpertSystem.java
@@ -4,6 +4,7 @@ import org.iucn.sis.shared.api.criteriacalculator.ExpertResult;
 import org.iucn.sis.shared.api.criteriacalculator.FuzzyExpImpl;
 import org.iucn.sis.shared.api.debug.Debug;
 import org.iucn.sis.shared.api.models.Assessment;
+import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
@@ -16,21 +17,49 @@ import files.Files;
 public class ExpertSystem extends BasicTest {
 	
 	@Test
-	public void run() {
-		Assessment assessment = getAssessment();
+	public void tryIt() {
+		Assessment assessment = getAssessment("A1854815");
 		
 		FuzzyExpImpl impl = new FuzzyExpImpl();
 		impl.VERBOSE = true;
 		
+		run(assessment, impl);
+	}
+	
+	@Test
+	public void badC2B() {
+		Assessment assessment = getAssessment("BadC2B");
+		
+		FuzzyExpImpl impl = new FuzzyExpImpl();
+		impl.VERBOSE = true;
+		
+		ExpertResult result = run(assessment, impl);
+		Assert.assertFalse("C2b".equals(result.getCriteriaString()));
+	}
+	
+	@Test
+	public void goodC2B() {
+		Assessment assessment = getAssessment("GoodC2B");
+		
+		FuzzyExpImpl impl = new FuzzyExpImpl();
+		impl.VERBOSE = true;
+		
+		ExpertResult result = run(assessment, impl);
+		Assert.assertTrue("C2b".equals(result.getCriteriaString()));
+	}
+	
+	private ExpertResult run(Assessment assessment, FuzzyExpImpl impl) {
 		ExpertResult result = impl.doAnalysis(assessment);
 		Debug.println("Result: " + result.getCriteriaString());
 		Debug.println("CR: " + result.getCriteriaStringCR());
 		Debug.println("EN: " + result.getCriteriaStringEN());
 		Debug.println("VU: " + result.getCriteriaStringVU());
+		
+		return result;
 	}
 	
-	private Assessment getAssessment() {
-		Document base = Files.getXML("A1854815.xml");
+	private Assessment getAssessment(String name) {
+		Document base = Files.getXML(name + ".xml");
 		
 		NativeDocument document = new JavaNativeDocument();
 		document.parse(BaseDocumentUtils.impl.serializeDocumentToString(base, true, false));
