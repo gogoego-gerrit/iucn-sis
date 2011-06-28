a6ea4e95d503eccb52476f61543b797919a37dd1
Carl Scott <carl.scott@solertium.com>
2011-06-28 18:17:52 -0400

    Additional Expert System test case coverage.
    Added additional test cases providing more code
    coverage for the Expert System, with tests derived
    from the language in the IUCN Red List Categories
    and Criteria Version 3.1 handbook (also available
    online).
    
    Also, changed from XML-read assessments to object-
    layer ones, hand-built, such that if the XML for
    assessments ever changes, there won't be a need to
    re-write the test cases.
    
    Change-Id: I9342d825c6990043ee0c60807e8bae40a08e4663
    Reviewed-on: http://review.source.gogoego.com/2537
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.server.testing/test/core/ExpertSystem.java
+++ org.iucn.sis.server.testing/test/core/ExpertSystem.java
@@ -1,9 +1,19 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.iucn.sis.shared.api.criteriacalculator.ExpertResult;
 import org.iucn.sis.shared.api.criteriacalculator.FuzzyExpImpl;
 import org.iucn.sis.shared.api.debug.Debug;
 import org.iucn.sis.shared.api.models.Assessment;
+import org.iucn.sis.shared.api.models.Field;
+import org.iucn.sis.shared.api.models.PrimitiveField;
+import org.iucn.sis.shared.api.models.primitivefields.BooleanRangePrimitiveField;
+import org.iucn.sis.shared.api.models.primitivefields.ForeignKeyListPrimitiveField;
+import org.iucn.sis.shared.api.models.primitivefields.RangePrimitiveField;
+import org.iucn.sis.shared.api.utils.CanonicalNames;
 import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -16,6 +26,8 @@ import files.Files;
 
 public class ExpertSystem extends BasicTest {
 	
+	static AtomicInteger idGenerator = new AtomicInteger(0);
+	
 	@Test
 	public void tryIt() {
 		Assessment assessment = getAssessment("A1854815");
@@ -27,8 +39,109 @@ public class ExpertSystem extends BasicTest {
 	}
 	
 	@Test
+	public void goodA1() {
+		Assessment assessment = new Assessment();
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPast, 
+			new RangePrimitiveField("range", null, "85")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastReversible, 
+			new BooleanRangePrimitiveField("value", null, "1")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastUnderstood, 
+			new BooleanRangePrimitiveField("value", null, "1")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastCeased, 
+			new BooleanRangePrimitiveField("value", null, "1")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastBasis, 
+			newFKList("value", 1, 4)	
+		));
+		
+		FuzzyExpImpl impl = new FuzzyExpImpl();
+		impl.VERBOSE = true;
+		
+		int index = 0;
+		String[] crit = new String[] { "CR", "EN", "VU", "DD" };
+		String[] cat = new String[] { "A1ad", "A1ad", "A1ad", "" };
+		for (String value : new String[] { "95", "85", "55", "45" }) {
+			Field field = assessment.getField(CanonicalNames.PopulationReductionPast);
+			field.getPrimitiveField("range").setRawValue(value);
+			
+			ExpertResult result = run(assessment, impl);
+			Assert.assertEquals(cat[index], result.getCriteriaString());
+			Assert.assertEquals(crit[index++], result.getAbbreviatedCategory());
+		}
+	}
+	
+	@Test
+	public void goodA2() {
+		Assessment assessment = new Assessment();
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPast, 
+			new RangePrimitiveField("range", null, "85")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastReversible, 
+			new BooleanRangePrimitiveField("value", null, "1")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastUnderstood, 
+			new BooleanRangePrimitiveField("value", null, "0")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastCeased, 
+			new BooleanRangePrimitiveField("value", null, "0")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionPastBasis, 
+			newFKList("value", 1, 4)
+		));
+		
+		FuzzyExpImpl impl = new FuzzyExpImpl();
+		impl.VERBOSE = true;
+		
+		int index = 0;
+		String[] crit = new String[] { "CR", "EN", "VU", "DD" };
+		String[] cat = new String[] { "A2ad", "A2ad", "A2ad", "" };
+		for (String value : new String[] { "85", "55", "35", "15" }) {
+			Field field = assessment.getField(CanonicalNames.PopulationReductionPast);
+			field.getPrimitiveField("range").setRawValue(value);
+			
+			ExpertResult result = run(assessment, impl);
+			Assert.assertEquals(cat[index], result.getCriteriaString());
+			Assert.assertEquals(crit[index++], result.getAbbreviatedCategory());
+		}
+	}
+	
+	@Test
+	public void goodA3() {
+		Assessment assessment = new Assessment();
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionFuture, 
+			new RangePrimitiveField("range", null, "85")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationReductionFutureBasis, 
+			newFKList("value", 3)
+		));
+		
+		FuzzyExpImpl impl = new FuzzyExpImpl();
+		impl.VERBOSE = true;
+		
+		int index = 0;
+		String[] crit = new String[] { "CR", "EN", "VU", "DD" };
+		String[] cat = new String[] { "A3d", "A3d", "A3d", "" };
+		for (String value : new String[] { "85", "55", "35", "15" }) {
+			Field field = assessment.getField(CanonicalNames.PopulationReductionFuture);
+			field.getPrimitiveField("range").setRawValue(value);
+			
+			ExpertResult result = run(assessment, impl);
+			Assert.assertEquals(cat[index], result.getCriteriaString());
+			Assert.assertEquals(crit[index++], result.getAbbreviatedCategory());
+		}
+	}
+	
+	@Test
 	public void badC2B() {
-		Assessment assessment = getAssessment("BadC2B");
+		Assessment assessment = new Assessment();
+		assessment.getField().add(newField(CanonicalNames.PopulationSize, 
+			new RangePrimitiveField("range", null, "230")));
+		assessment.getField().add(newField(CanonicalNames.PopulationExtremeFluctuation, 
+			new BooleanRangePrimitiveField("isFluctuating", null, "1")	
+		));
 		
 		FuzzyExpImpl impl = new FuzzyExpImpl();
 		impl.VERBOSE = true;
@@ -39,7 +152,15 @@ public class ExpertSystem extends BasicTest {
 	
 	@Test
 	public void goodC2B() {
-		Assessment assessment = getAssessment("GoodC2B");
+		Assessment assessment = new Assessment();
+		assessment.getField().add(newField(CanonicalNames.PopulationSize, 
+			new RangePrimitiveField("range", null, "230")));
+		assessment.getField().add(newField(CanonicalNames.PopulationExtremeFluctuation, 
+			new BooleanRangePrimitiveField("isFluctuating", null, "1")	
+		));
+		assessment.getField().add(newField(CanonicalNames.PopulationContinuingDecline,
+			new BooleanRangePrimitiveField("isDeclining", null, "1")
+		));
 		
 		FuzzyExpImpl impl = new FuzzyExpImpl();
 		impl.VERBOSE = true;
@@ -48,12 +169,79 @@ public class ExpertSystem extends BasicTest {
 		Assert.assertTrue("C2b".equals(result.getCriteriaString()));
 	}
 	
+	@Test
+	public void goodE() {
+		Assessment assessment = new Assessment();
+		assessment.getField().add(newField(CanonicalNames.ExtinctionProbabilityGenerations3,
+			new RangePrimitiveField("range", null, "55")
+		));
+		assessment.getField().add(newField(CanonicalNames.ExtinctionProbabilityGenerations5,
+			new RangePrimitiveField("range", null, "25")
+		));
+		assessment.getField().add(newField(CanonicalNames.ExtinctionProbabilityYears100,
+			new RangePrimitiveField("range", null, "15")
+		));
+		
+		FuzzyExpImpl impl = new FuzzyExpImpl();
+		impl.VERBOSE = true;
+		
+		String[] cat = new String[] { "CR", "EN", "VU", "LC" };
+		String[] crit = new String[] { "E", "E", "E", "" };
+		
+		for (int i = 0; i < crit.length; i++) {
+			if (i == 1) 
+				assessment.getField().remove(assessment.getField(CanonicalNames.ExtinctionProbabilityGenerations3));
+			else if (i == 2)
+				assessment.getField().remove(assessment.getField(CanonicalNames.ExtinctionProbabilityGenerations5));
+			else if (i == 3)
+				assessment.getField(CanonicalNames.ExtinctionProbabilityYears100).getPrimitiveField("range").setRawValue("5");
+			
+			ExpertResult result = run(assessment, impl);
+			
+			Assert.assertEquals(cat[i], result.getAbbreviatedCategory());
+			Assert.assertEquals(crit[i], result.getCriteriaString());
+		}
+		
+		
+		
+	}
+	
+	private Field newField(String name, PrimitiveField<?>... prims) {
+		Field field = new Field();
+		field.setId(idGenerator.incrementAndGet());
+		field.setName(name);
+		
+		for (PrimitiveField<?> prim : prims) {
+			prim.setField(field);
+			field.addPrimitiveField(prim);
+		}
+		
+		return field;
+	}
+	
+	private ForeignKeyListPrimitiveField newFKList(String name, int... values) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int value : values)
+			list.add(value);
+		
+		ForeignKeyListPrimitiveField prim = new ForeignKeyListPrimitiveField();
+		prim.setName(name);
+		prim.setValue(list);
+		
+		return prim;
+		
+	}
+	
 	private ExpertResult run(Assessment assessment, FuzzyExpImpl impl) {
+		System.out.println("----- Begin Test -----");
+		
 		ExpertResult result = impl.doAnalysis(assessment);
 		Debug.println("Result: " + result.getCriteriaString());
 		Debug.println("CR: " + result.getCriteriaStringCR());
 		Debug.println("EN: " + result.getCriteriaStringEN());
 		Debug.println("VU: " + result.getCriteriaStringVU());
+		
+		System.out.println("----- Done Test -----");
 		
 		return result;
 	}
