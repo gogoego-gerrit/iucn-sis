37beb058ff1985d0606bb9e3cc87d0770e2840df
Carl Scott <carl.scott@solertium.com>
2011-08-20 12:39:03 -0400

    Corrected and cleaned up vw_filter query.
    Temporary table no longer needed for the vw_filter
    creation script, also included the hidden table and
    combined both queries into one query that directly
    populates the vw_filter table.
    
    See #562
    
    Change-Id: I5905768f7b42e8ba0550a395959840252762a932
    Reviewed-on: http://review.source.gogoego.com/2961
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- viewmaster/org.iucn.sis.viewmaster/src/org/iucn/sis/viewmaster/RedListPublish_2.sql
+++ viewmaster/org.iucn.sis.viewmaster/src/org/iucn/sis/viewmaster/RedListPublish_2.sql
@@ -1,17 +1,27 @@
-CREATE TEMPORARY TABLE _published_1 AS
-  SELECT DISTINCT t2.taxonid, t2.assessmentid
-  FROM (
-    SELECT DISTINCT taxonid, assessmentid, value 
-    FROM public.vw_redlistassessmentdate_value
-  ) t2
-  JOIN public.vw_regioninformation_regions ON public.vw_regioninformation_regions.assessmentid = t2.assessmentid
-  JOIN public.assessment ON public.assessment.id = t2.assessmentid
-  JOIN public.taxon ON public.taxon.id = public.assessment.id
-  LEFT JOIN public.vw_redlisthidden_value ON public.vw_redlisthidden_value.assessmentid = t2.assessmentid
+DELETE FROM $schema.vw_filter;
+INSERT INTO $schema.vw_filter
+ SELECT DISTINCT t2.taxonid, t2.assessmentid
+  FROM
+  public.vw_redlistassessmentdate_value t2,
+  public.vw_regioninformation_regions,
+  public.vw_redlistcriteria,
+  public.assessment,
+  public.taxon
   WHERE 
-  public.vw_redlisthidden_value.value is null AND
+  t2.assessmentid = public.vw_redlistcriteria.assessmentid AND
+  t2.assessmentid = public.assessment.id AND
+  public.assessment.taxonid = public.taxon.id AND
+  public.assessment.id = public.vw_regioninformation_regions.assessmentid AND
   public.assessment.assessment_typeid=1 AND
+  public.assessment.state = 0 AND
   public.taxon.taxon_statusid in(1,4) AND
+  t2.value >= '1996-01-01' AND
+  Case 	When IsManual=True then manualCategory <>'NE'
+	else autoCategory <>'NE'	
+  End AND
+  t2.assessmentid NOT IN (
+    SELECT assessmentid FROM vw_redlisthidden_value
+  ) AND
   (t2.taxonid,t2.value,public.vw_regioninformation_regions.value) IN (
     SELECT t1.taxonid, MAX(t1.value), public.vw_regioninformation_regions.value
     FROM public.vw_redlistassessmentdate_value as t1,
@@ -25,21 +35,3 @@ CREATE TEMPORARY TABLE _published_1 AS
     public.taxon.taxon_statusid in(1,4)
     GROUP BY t1.taxonid, public.vw_regioninformation_regions.value
   );
-
-DELETE FROM $schema.vw_filter;
-INSERT INTO $schema.vw_filter
-SELECT 
-  _published_1.taxonid, 
-  _published_1.assessmentid
-FROM 
-  _published_1, 
-  public.vw_redlistcriteria,
-  (SELECT DISTINCT taxonid, assessmentid, value FROM public.vw_redlistassessmentdate_value) as t2
-WHERE 
-  _published_1.assessmentid = public.vw_redlistcriteria.assessmentid AND
-  _published_1.assessmentid=t2.assessmentid AND
-  t2.value>='1996-01-01' AND
-  
-Case 	When IsManual=True then manualCategory <>'NE'
-	else autoCategory <>'NE'	
-End;
