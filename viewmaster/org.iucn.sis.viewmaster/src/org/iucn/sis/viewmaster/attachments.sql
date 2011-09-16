708b8b5eaffc503633146dec1c42337e7c99e0ea
Carl Scott <carl.scott@solertium.com>
2011-09-16 12:57:51 -0400

    Update attachment queries to include schema param
    Was looking at a particular schema instead of injecting
    it based on properties.
    
    Change-Id: If08fb68c076b657dac510203394f602e07f0dc10
    Reviewed-on: http://review.source.gogoego.com/3138
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- viewmaster/org.iucn.sis.viewmaster/src/org/iucn/sis/viewmaster/attachments.sql
+++ viewmaster/org.iucn.sis.viewmaster/src/org/iucn/sis/viewmaster/attachments.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW $schema.vw_taxonomicnotes_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'TaxonomicNotes'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -10,7 +10,7 @@ GRANT SELECT ON $schema.vw_taxonomicnotes_attachments TO $user;
 
 CREATE OR REPLACE VIEW $schema.vw_habitatdocumentation_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'HabitatDocumentation'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -20,7 +20,7 @@ GRANT SELECT ON $schema.vw_habitatdocumentation_attachments TO $user;
 
 CREATE OR REPLACE VIEW $schema.vw_populationdocumentation_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'PopulationDocumentation'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -30,7 +30,7 @@ GRANT SELECT ON $schema.vw_populationdocumentation_attachments TO $user;
 
 CREATE OR REPLACE VIEW $schema.vw_rangedocumentation_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'RangeDocumentation'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -40,7 +40,7 @@ GRANT SELECT ON $schema.vw_rangedocumentation_attachments TO $user;
 
 CREATE OR REPLACE VIEW $schema.vw_threatsdocumentation_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'ThreatsDocumentation'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -51,7 +51,7 @@ GRANT SELECT ON $schema.vw_threatsdocumentation_attachments TO $user;
 
 CREATE OR REPLACE VIEW $schema.vw_conservationactionsdocumentation_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'ConservationActionsDocumentation'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -61,7 +61,7 @@ GRANT SELECT ON $schema.vw_conservationactionsdocumentation_attachments TO $user
 
 CREATE OR REPLACE VIEW $schema.vw_usetradedocumentation_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'UseTradeDocumentation'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
@@ -71,7 +71,7 @@ GRANT SELECT ON $schema.vw_usetradedocumentation_attachments TO $user;
 
 CREATE OR REPLACE VIEW $schema.vw_redlistrationale_attachments AS 
  SELECT f.taxonid, f.assessmentid, a.name, a.key AS url
-   FROM vw_published.vw_filter f
+   FROM $schema.vw_filter f
    JOIN public.field i ON i.assessmentid = f.assessmentid AND i.name::text = 'RedListRationale'::text
    JOIN public.fieldattachment_field ai ON ai.fieldid = i.id
    JOIN public.fieldattachment a ON a.id = ai.fieldattachmentid
