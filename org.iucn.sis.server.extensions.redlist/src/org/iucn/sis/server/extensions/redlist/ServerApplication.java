84619f41cbc78f0d0dd7d64888bacc61730967fb
Carl Scott <carl.scott@solertium.com>
2011-09-19 14:53:23 -0400

    Show configuration params on settings page.
    Made available the configuration parameter for the
    image upload URL on the GoGoEgo settings page.
    
    Change-Id: Ib0da053719e058016368042b06bdacd3ccfa6bb5
    Reviewed-on: http://review.source.gogoego.com/3152
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.server.extensions.redlist/src/org/iucn/sis/server/extensions/redlist/ServerApplication.java
+++ org.iucn.sis.server.extensions.redlist/src/org/iucn/sis/server/extensions/redlist/ServerApplication.java
@@ -1,5 +1,9 @@
 package org.iucn.sis.server.extensions.redlist;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.iucn.sis.server.api.application.SimpleSISApplication;
 
 public class ServerApplication extends SimpleSISApplication {
@@ -14,6 +18,13 @@ public class ServerApplication extends SimpleSISApplication {
 	@Override
 	public void init() {
 		addServiceToRouter(new RedlistRestlet(app.getContext()));
+	}
+	
+	@Override
+	protected Collection<String> getSettingsKeys() {
+		List<String> keys = new ArrayList<String>();
+		keys.add("org.iucn.sis.server.extensions.redlist.imagePublishURL");
+		return keys;
 	}
 
 }
