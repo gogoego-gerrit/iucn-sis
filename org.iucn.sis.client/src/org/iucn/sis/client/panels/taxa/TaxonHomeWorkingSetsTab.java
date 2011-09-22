b9aa2cf8ff70301141487341a3b5820f797e5966
Carl Scott <carl.scott@solertium.com>
2011-09-22 16:14:12 -0400

    Taxon Home Working Sets tab improvements
    * Proper sorting of working sets by name
    * Explicit jump button appears for authorized working sets.
    * RowClick still works -- but only for assessments you can open.
    
    Ready to Deploy #611
    
    Change-Id: If3130378f837b6e869de3cc4ecaac18b2f78ed2e
    Reviewed-on: http://review.source.gogoego.com/3164
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.client/src/org/iucn/sis/client/panels/taxa/TaxonHomeWorkingSetsTab.java
+++ org.iucn.sis.client/src/org/iucn/sis/client/panels/taxa/TaxonHomeWorkingSetsTab.java
@@ -3,23 +3,31 @@ package org.iucn.sis.client.panels.taxa;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.iucn.sis.client.api.caches.FetchMode;
 import org.iucn.sis.client.api.caches.TaxonomyCache;
 import org.iucn.sis.client.api.caches.WorkingSetCache;
 import org.iucn.sis.client.api.container.StateManager;
 import org.iucn.sis.shared.api.models.WorkingSet;
+import org.iucn.sis.shared.api.utils.CaseInsensitiveAlphanumericComparator;
 
 import com.extjs.gxt.ui.client.Style.SortDir;
 import com.extjs.gxt.ui.client.data.BaseModelData;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.GridEvent;
+import com.extjs.gxt.ui.client.event.IconButtonEvent;
 import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.store.StoreSorter;
 import com.extjs.gxt.ui.client.widget.Component;
 import com.extjs.gxt.ui.client.widget.Info;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.button.IconButton;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.grid.ColumnData;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
 import com.extjs.gxt.ui.client.widget.layout.FillLayout;
 import com.solertium.lwxml.shared.GenericCallback;
 import com.solertium.util.extjs.client.WindowUtils;
@@ -51,61 +59,62 @@ public class TaxonHomeWorkingSetsTab extends LayoutContainer implements DrawsLaz
 	private void loadWorkingSetInformationPanel(final DrawsLazily.DoneDrawingCallbackWithParam<Component> callback) {
 		TaxonomyCache.impl.fetchWorkingSetsForTaxon(TaxonomyCache.impl.getCurrentTaxon(), new GenericCallback<List<WorkingSet>>() {
 			public void onSuccess(List<WorkingSet> workingSets) {
-				/*final ContentPanel workingSetInformation = new ContentPanel(new FillLayout());
-				workingSetInformation.setStyleName("x-panel");
-				workingSetInformation.setWidth(350);
-				workingSetInformation.setHeight(200);
-				workingSetInformation.setHeading("Related Working Sets");
-				workingSetInformation.setLayoutOnChange(true);
-				workingSetInformation.setScrollMode(Scroll.AUTO);
+				final ListStore<BaseModelData> store = new ListStore<BaseModelData>();
+				store.setStoreSorter(new StoreSorter<BaseModelData>(new CaseInsensitiveAlphanumericComparator()));
 				
-				if (workingSets.size() > 0) {*/
+				for (final WorkingSet data : workingSets) {
+					BaseModelData model = new BaseModelData();
+					model.set("wsname", data.getName());
+					model.set("creator", data.getCreatorUsername());
+					model.set("id", data.getId());
+					model.set("open", WorkingSetCache.impl.isCached(data.getId(), FetchMode.PARTIAL));
+					store.add(model);
+				}
 
-					final ListStore<BaseModelData> store = new ListStore<BaseModelData>();
-					
-					for (final WorkingSet data : workingSets) {
-						BaseModelData model = new BaseModelData();
-						model.set("wsname", data.getName());
-						model.set("creator", data.getCreatorUsername());
-						model.set("id", data.getId());
-						store.add(model);
-					}
+				List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
 
-					List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
-
-					columns.add(new ColumnConfig("wsname", "Working Set Name", 200));
-					columns.add(new ColumnConfig("creator", "Owner", 150));
-					
-					final Grid<BaseModelData> tbl = new Grid<BaseModelData>(store, new ColumnModel(columns));
-					
-					tbl.setBorders(false);
-					tbl.removeAllListeners();
-					tbl.addListener(Events.RowClick, new Listener<GridEvent<BaseModelData>>() {
-						public void handleEvent(GridEvent<BaseModelData> be) {
+				ColumnConfig openColumn = new ColumnConfig("open", "Open", 50);
+				openColumn.setSortable(false);
+				openColumn.setRenderer(new GridCellRenderer<BaseModelData>() {
+					public Object render(final BaseModelData model, String property, ColumnData config,
+							int rowIndex, int colIndex, ListStore<BaseModelData> store,
+							Grid<BaseModelData> grid) {
+						boolean open = model.get(property);
+						if (open) {
+							IconButton openIcon = new IconButton("icon-go-jump");
+							openIcon.addSelectionListener(new SelectionListener<IconButtonEvent>() {
+								public void componentSelected(IconButtonEvent ce) {
+									openWorkingSet(model);
+								}
+							});
 							
-							if (be.getModel() == null)
-								return;
-
-							BaseModelData model = be.getModel();
-							final Integer id = model.get("id");
-
-							//WorkingSetCache.impl.setCurrentWorkingSet(id, true);
-							StateManager.impl.setState(WorkingSetCache.impl.getWorkingSet(id), null, null);
-							//ClientUIContainer.bodyContainer.setSelection(ClientUIContainer.bodyContainer.tabManager.workingSetPage);
-
+							return openIcon;
 						}
-					});
-					
-					//ClientUIContainer.headerContainer.update();
-					tbl.getStore().sort("wsname", SortDir.ASC);
-					/*workingSetInformation.add(tbl);
-				}else*/
-					//workingSetInformation.add(new HTML("There are no working sets for this taxon."));
-					
-					if (workingSets.isEmpty())
-						Info.display("Info", "There are no working sets for this taxon.");
-					
+						else
+							return "";
+					}
+				});
 				
+				columns.add(new ColumnConfig("wsname", "Working Set Name", 200));
+				columns.add(new ColumnConfig("creator", "Owner", 150));
+				columns.add(openColumn);
+				
+				final Grid<BaseModelData> tbl = new Grid<BaseModelData>(store, new ColumnModel(columns));
+				tbl.setBorders(false);
+				tbl.removeAllListeners();
+				tbl.addListener(Events.RowClick, new Listener<GridEvent<BaseModelData>>() {
+					@Override
+					public void handleEvent(GridEvent<BaseModelData> be) {
+						if (be.getModel() != null && Boolean.TRUE.equals(be.getModel().get("open")))
+							openWorkingSet(be.getModel());
+					}
+				});
+					
+				tbl.getStore().sort("wsname", SortDir.ASC);
+					
+				if (workingSets.isEmpty())
+					Info.display("Info", "There are no working sets for this taxon.");
+					
 				callback.isDrawn(tbl);
 			}
 			public void onFailure(Throwable caught) {
@@ -114,4 +123,9 @@ public class TaxonHomeWorkingSetsTab extends LayoutContainer implements DrawsLaz
 		});
 	}
 
+	private void openWorkingSet(BaseModelData model) {
+		final Integer id = model.get("id");
+		StateManager.impl.setState(WorkingSetCache.impl.getWorkingSet(id), null, null);
+	}
+	
 }
