f7aa3530e14a277d90b253f5841f96c5a052ad25
Carl Scott <carl.scott@solertium.com>
2011-10-13 18:40:27 -0400

    Display upgrades for SISOneToMany structures.
    Changed the one-to-many structure to use popup
    widnows and listings to display records instead of
    an infinitely-scrolling page.  These records shall
    now also support notes and references, and should
    be easier to view and navigate the data.
    
    The components involved in saving and loading have
    remained the same, this is more of a facelift,
    with new functionality added.
    
    While not complete, this commit should be considered
    a "status quo" offering -- that is, things should
    work just as well as they did before.  There are still
    some tweaks left in regard to notes & references and
    some naming conventions.
    
    See #238
    
    Change-Id: I7be5e0c84da7029388d3a58eeb0036bc4ed72947
    Reviewed-on: http://review.source.gogoego.com/3230
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.client.shared/src/org/iucn/sis/shared/api/structures/SISOneToMany.java
+++ org.iucn.sis.client.shared/src/org/iucn/sis/shared/api/structures/SISOneToMany.java
@@ -2,7 +2,6 @@ package org.iucn.sis.shared.api.structures;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -10,19 +9,16 @@ import org.iucn.sis.shared.api.data.DisplayData;
 import org.iucn.sis.shared.api.data.DisplayDataProcessor;
 import org.iucn.sis.shared.api.debug.Debug;
 import org.iucn.sis.shared.api.models.Field;
+import org.iucn.sis.shared.api.structures.SISOneToManyWindow.StructureHolder;
 
 import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.Listener;
-import com.extjs.gxt.ui.client.event.MessageBoxEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.event.WindowEvent;
+import com.extjs.gxt.ui.client.event.WindowListener;
 import com.extjs.gxt.ui.client.widget.button.Button;
-import com.google.gwt.user.client.ui.CellPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.solertium.util.extjs.client.WindowUtils;
 
 /**
  * This structure allows the user to create many instances of a single structure
@@ -33,23 +29,19 @@ import com.solertium.util.extjs.client.WindowUtils;
 @SuppressWarnings("unchecked")
 public class SISOneToMany extends Structure<Field> {
 
-	/**
-	 * ArrayList<Structure>
-	 */
-	private ArrayList<StructureHolder> selected;
-
-	private DisplayData defaultStructureData;
-
-	private Button addNew;
-
-	private VerticalPanel selectedPanel;
+	private SISOneToManyWindow oneToMany;
+	private HTML recordCount;
 
 	public SISOneToMany(String struct, String descript, String structID, DisplayData defaultStructure) {
 		super(struct, descript, structID);
 		buildContentPanel(Orientation.VERTICAL);
-
-		selected = new ArrayList<StructureHolder>();
-		defaultStructureData = defaultStructure;
+		
+		oneToMany = new SISOneToManyWindow(descript, defaultStructure);
+		oneToMany.addWindowListener(new WindowListener() {
+			public void windowHide(WindowEvent we) {
+				updateRecordCount();
+			}
+		});
 	}
 	
 	@Override
@@ -59,11 +51,11 @@ public class SISOneToMany extends Structure<Field> {
 			for (Field subfield : field.getFields())
 				savedFields.put(subfield.getId(), subfield);
 		
-		for (StructureHolder holder : selected) {
+		for (StructureHolder holder : oneToMany.getSelected()) {
 			//Field has never been saved, if there are changes, let's save.
-			if (holder.field == null || holder.field.getId() == 0) {
-				if (holder.structure.hasChanged(holder.field)) {
-					Debug.println("NEW: {0} says it has changed", holder.structure.getClass().getName());
+			if (holder.getField() == null || holder.getField().getId() == 0) {
+				if (holder.hasChanged()) {
+					Debug.println("NEW: {0} says it has changed", holder.getStructure().getClass().getName());
 					return true;
 				}
 				else
@@ -71,14 +63,14 @@ public class SISOneToMany extends Structure<Field> {
 			}
 			else {
 				//We are working with previously saved data that could have changed.
-				Field dataField = savedFields.remove(holder.field.getId());
+				Field dataField = savedFields.remove(holder.getField().getId());
 				if (dataField == null) { //How??
 					Debug.println("OneToMany hasChanged badness, save to clean up.");
 					return true;
 				}
 				
-				if (holder.structure.hasChanged(dataField)) {
-					Debug.println("OLD: {0} says it has changed", holder.structure.getClass().getName());
+				if (holder.getStructure().hasChanged(dataField)) {
+					Debug.println("OLD: {0} says it has changed", holder.getStructure().getClass().getName());
 					return true;
 				}
 			}
@@ -98,34 +90,34 @@ public class SISOneToMany extends Structure<Field> {
 		
 		final List<StructureHolder> unsaved = new ArrayList<StructureHolder>();
 		
-		for (StructureHolder cur : selected) {
-			if (cur.field == null)
+		for (StructureHolder cur : oneToMany.getSelected()) {
+			if (cur.getField() == null)
 				unsaved.add(cur);
 			else
-				cur.structure.save(field, cur.field);
+				cur.getStructure().save(field, cur.getField());
 		}
 		
 		for (StructureHolder cur : unsaved) {
 			Field subfield = new Field(field.getName() + "Subfield", field.getAssessment());
 			subfield.setParent(field);
 			
-			cur.structure.save(field, subfield);
+			cur.getStructure().save(field, subfield);
 			
-			field.addField(subfield);
+			field.getFields().add(subfield);
 			
-			cur.field = subfield;
+			cur.setField(subfield);
 		}
 	}
 
 	@Override
 	public void clearData() {
-		selected.clear();
+		oneToMany.clear();
 	}
 
 	@Override
 	protected Widget createLabel() {
 		clearDisplayPanel();
-		selectedPanel.clear();
+		/*selectedPanel.clear();
 
 		for (final StructureHolder curStruct : selected) {
 			HorizontalPanel structWrapper = new HorizontalPanel();
@@ -154,47 +146,54 @@ public class SISOneToMany extends Structure<Field> {
 			selectedPanel.add(structWrapper);
 		}
 
-		selectedPanel.add(addNew);
+		selectedPanel.add(addNew);*/
+		
+		updateRecordCount();
 
 		displayPanel.add(descriptionLabel);
-		displayPanel.add(selectedPanel);
+		displayPanel.add(recordCount);
+		displayPanel.add(new Button("View/Edit", new SelectionListener<ButtonEvent>() {
+			public void componentSelected(ButtonEvent ce) {
+				oneToMany.createLabel(false);
+			}
+		}));
 		return displayPanel;
 	}
 
 	@Override
 	protected Widget createViewOnlyLabel() {
 		clearDisplayPanel();
-		selectedPanel.clear();
-		((CellPanel) displayPanel).setSpacing(2);
-
-		if (selected.size() == 0)
-			selectedPanel.add(new HTML("No information available."));
-		else
-			for (Iterator<StructureHolder> iter = selected.listIterator(); iter.hasNext();)
-				selectedPanel.add(iter.next().structure.createViewOnlyLabel());
-
+		
+		updateRecordCount();
+		
 		displayPanel.add(descriptionLabel);
-		displayPanel.add(selectedPanel);
+		displayPanel.add(recordCount);
+		displayPanel.add(new Button("View", new SelectionListener<ButtonEvent>() {
+			public void componentSelected(ButtonEvent ce) {
+				oneToMany.createLabel(false);
+			}
+		}));
 		return displayPanel;
+	}
+	
+	private void updateRecordCount() {
+		int size = oneToMany.getSelected().size();
+		
+		String description = this.description;
+		if (!(description == null || "".equals(description)))
+			description = " " + description;
+		
+		recordCount.setHTML(size == 0 ? "No" + description + " records." : 
+			size == 1 ? "One" + description + " record." : 
+			size + description + " records");
 	}
 
 	@Override
 	public void createWidget() {
-		selectedPanel = new VerticalPanel();
-
 		descriptionLabel = new HTML(description);
 		descriptionLabel.addStyleName("bold");
-
-		addNew = new Button("Add");
-		addNew.setIconStyle("icon-add");
-		addNew.addSelectionListener(new SelectionListener<ButtonEvent>() {
-			public void componentSelected(ButtonEvent ce) {
-				Structure newOne = DisplayDataProcessor.processDisplayStructure(defaultStructureData);
-				selected.add(new StructureHolder(newOne));
-
-				createLabel();
-			}
-		});
+		
+		recordCount = new HTML();
 	}
 
 	/**
@@ -204,7 +203,7 @@ public class SISOneToMany extends Structure<Field> {
 	@Override
 	public ArrayList<String> extractDescriptions() {
 		Structure structureData = 
-			DisplayDataProcessor.processDisplayStructure(defaultStructureData);
+			DisplayDataProcessor.processDisplayStructure(oneToMany.getDisplayData());
 		
 		ArrayList<String> ret = new ArrayList<String>();
 		ret.addAll(structureData.extractDescriptions());
@@ -223,10 +222,6 @@ public class SISOneToMany extends Structure<Field> {
 	public String getData() {
 		return null;
 	}
-
-	public DisplayData getDefaultStructureData() {
-		return defaultStructureData;
-	}
 	
 	/**
 	 * Pass in the raw data from an Assessment object, and this will return
@@ -243,7 +238,7 @@ public class SISOneToMany extends Structure<Field> {
 		offset++;
 
 		Structure def = 
-			DisplayDataProcessor.processDisplayStructure(defaultStructureData);
+			DisplayDataProcessor.processDisplayStructure(oneToMany.getDisplayData());
 
 		for (int i = 0; i < num; i++)
 			offset = def.getDisplayableData(rawData, prettyData, offset);
@@ -253,34 +248,13 @@ public class SISOneToMany extends Structure<Field> {
 	
 	@Override
 	public void setData(Field field) {
-		selected.clear();
-		
-		if (field != null)
-			for (Field subField : field.getFields())
-				selected.add(new StructureHolder(DisplayDataProcessor.processDisplayStructure(defaultStructureData), subField));
+		oneToMany.setData(field);
 	}
 	
 	public void setEnabled(boolean isEnabled) {
-		for (Iterator<StructureHolder> iter = selected.listIterator(); iter.hasNext();)
-			iter.next().structure.setEnabled(isEnabled);
+		oneToMany.setEnabled(isEnabled);
 	}
 	
-	public static class StructureHolder {
-		
-		private Structure<Object> structure;
-		private Field field;
-		
-		public StructureHolder(Structure<Object> structure) {
-			this(structure, null);
-		}
-		
-		public StructureHolder(Structure<Object> structure, Field field) {
-			this.structure = structure;
-			this.field = field;
-			
-			this.structure.setData(field);
-		}
-		
-	}
+	
 	
 }
---
+++ org.iucn.sis.client.shared/src/org/iucn/sis/shared/api/structures/SISOneToManyWindow.java
@@ -0,0 +1,430 @@
+package org.iucn.sis.shared.api.structures;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.iucn.sis.client.api.assessment.ReferenceableField;
+import org.iucn.sis.client.api.caches.AssessmentCache;
+import org.iucn.sis.client.api.container.SISClientBase;
+import org.iucn.sis.client.api.ui.notes.NotesWindow;
+import org.iucn.sis.client.api.utils.BasicWindow;
+import org.iucn.sis.shared.api.data.DisplayData;
+import org.iucn.sis.shared.api.data.DisplayDataProcessor;
+import org.iucn.sis.shared.api.displays.ClassificationScheme;
+import org.iucn.sis.shared.api.displays.FieldNotes;
+import org.iucn.sis.shared.api.models.Field;
+import org.iucn.sis.shared.api.models.Notes;
+
+import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.Style.LayoutRegion;
+import com.extjs.gxt.ui.client.Style.Scroll;
+import com.extjs.gxt.ui.client.Style.SelectionMode;
+import com.extjs.gxt.ui.client.Style.SortDir;
+import com.extjs.gxt.ui.client.data.BaseModelData;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.IconButtonEvent;
+import com.extjs.gxt.ui.client.event.MenuEvent;
+import com.extjs.gxt.ui.client.event.SelectionChangedEvent;
+import com.extjs.gxt.ui.client.event.SelectionChangedListener;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.store.StoreSorter;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.Html;
+import com.extjs.gxt.ui.client.widget.HtmlContainer;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.ListView;
+import com.extjs.gxt.ui.client.widget.ListViewSelectionModel;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.button.ButtonBar;
+import com.extjs.gxt.ui.client.widget.button.IconButton;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
+import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
+import com.extjs.gxt.ui.client.widget.layout.FillLayout;
+import com.extjs.gxt.ui.client.widget.menu.Menu;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.extjs.gxt.ui.client.widget.toolbar.FillToolItem;
+import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
+import com.solertium.lwxml.shared.GenericCallback;
+import com.solertium.util.events.SimpleListener;
+import com.solertium.util.extjs.client.WindowUtils;
+import com.solertium.util.portable.PortableAlphanumericComparator;
+
+public class SISOneToManyWindow extends BasicWindow {
+	
+	private final DisplayData defaultStructureData;
+	
+	private final ListView<BaseModelData> view; 
+	private final ListStore<BaseModelData> store;
+	
+	private final Html fullNameDisplay;
+	
+	private final LayoutContainer center;
+	private final LayoutContainer editingArea;
+	
+	private LayoutContainer completeEditingArea;
+	
+	private StructureHolder current;
+	
+	private boolean viewOnly = false;
+	
+	public SISOneToManyWindow(String heading, DisplayData defaultStructure) {
+		super(heading);
+		setLayout(new FillLayout());
+		setSize(ClassificationScheme.WINDOW_WIDTH, ClassificationScheme.WINDOW_HEIGHT);
+		
+		this.defaultStructureData = defaultStructure;
+		
+		this.store = new ListStore<BaseModelData>();
+		this.store.setStoreSorter(new StoreSorter<BaseModelData>(new PortableAlphanumericComparator()));
+		
+		this.view = new ListView<BaseModelData>(store);
+		this.view.setSimpleTemplate("<span class=\"{style}\">Record #{id}</span>");
+		
+		this.editingArea = new LayoutContainer();
+		this.editingArea.setLayoutOnChange(true);
+		this.editingArea.setScrollMode(Scroll.AUTO);
+		
+		this.fullNameDisplay = new Html();
+		this.center = new LayoutContainer(new FillLayout());
+		this.center.setLayoutOnChange(true);
+		
+		center.add(completeEditingArea = createEditingArea());
+		
+		final ContentPanel listing = new ContentPanel();
+		listing.setBodyBorder(false);
+		listing.setBorders(false);
+		listing.setHeading("Records");
+		listing.setLayout(new FillLayout());
+		listing.add(createListingArea());
+		
+		final BorderLayoutData listingArea = 
+			new BorderLayoutData(LayoutRegion.WEST, 150, 150, 150);
+		listingArea.setCollapsible(true);
+		
+		final LayoutContainer container = new LayoutContainer(new BorderLayout());
+		container.add(listing, listingArea);
+		container.add(center, new BorderLayoutData(LayoutRegion.CENTER));
+		
+		add(container);
+	}
+	
+	public void createLabel(boolean viewOnly) {
+		this.viewOnly = viewOnly;
+		
+		for (BaseModelData model : store.getModels()) {
+			StructureHolder holder = model.get("model");
+			model.set("id", holder.updateId());
+		}
+		
+		view.refresh();
+		
+		stopEditing();
+		
+		show();
+	}
+	
+	private void setCurrent(StructureHolder holder) {
+		center.removeAll();
+		center.add(completeEditingArea);
+		
+		this.current = holder;
+		
+		this.fullNameDisplay.setHtml("Record #" + holder.getId());
+		
+		editingArea.removeAll();
+		if (viewOnly)
+			editingArea.add(holder.structure.createViewOnlyLabel());
+		else
+			editingArea.add(holder.structure.createLabel());
+	}
+	
+	private void stopEditing() {
+		center.removeAll();
+		fullNameDisplay.setHtml("");
+		view.getSelectionModel().deselectAll();
+		
+		current = null;
+		
+		showDefaultScreen();
+	}
+	
+	private void showDefaultScreen() {
+		final LayoutContainer container = new LayoutContainer(new CenterLayout());
+		
+		final HtmlContainer instructions = new HtmlContainer();
+		instructions.addStyleName("gwt-background");
+		instructions.setSize(200, 100);
+		instructions.setHtml("<b>Instructions</b>: Select " +
+			"the record from the list on the left which you would " +
+			"like to edit, or click \"Add\" to create a new record.");
+		instructions.setBorders(true);
+		
+		container.add(instructions);
+		
+		center.removeAll();
+		center.add(container);
+	}
+	
+	private Menu buildOptionsMenu() {
+		Menu optionsMenu = new Menu();
+		
+		boolean hasAssessment = AssessmentCache.impl.getCurrentAssessment() != null;
+		
+		if (hasAssessment) {
+			String notesIconStyle;
+			Set<Notes> notes = current.getField().getNotes();
+			if (notes == null || notes.isEmpty())
+				notesIconStyle = ("images/icon-note-grey.png");
+			else
+				notesIconStyle = ("images/icon-note.png");
+			
+			MenuItem notesMenu = new MenuItem("Notes");
+			notesMenu.setIconStyle(notesIconStyle);
+			notesMenu.addSelectionListener(new SelectionListener<MenuEvent>() {
+				public void componentSelected(MenuEvent ce) {
+					if (current.getField() == null || !(current.getField().getId() > 0))
+						WindowUtils.errorAlert("Please save your changes before adding notes.");
+					else {
+						NotesWindow window = new NotesWindow(new FieldNotes(current.getField()) {
+							public void onClose(List<Notes> list) {
+								
+							}
+						});
+						window.show();
+					}
+				}
+			});
+			optionsMenu.add(notesMenu);
+		}
+		
+		String referencesIconStyle;
+		if (current != null && !current.getField().getReference().isEmpty())
+			referencesIconStyle = "images/icon-book.png";
+		else
+			referencesIconStyle = "images/icon-book-grey.png";
+		
+		MenuItem referenceMenu = new MenuItem("References");
+		referenceMenu.setIconStyle(referencesIconStyle);
+		referenceMenu.addSelectionListener(new SelectionListener<MenuEvent>() {
+			public void componentSelected(MenuEvent ce) {
+				GenericCallback<Object> callback = new GenericCallback<Object>() {
+					public void onFailure(Throwable caught) {
+						WindowUtils.errorAlert("Error saving changes, please try again.");
+					}
+					public void onSuccess(Object result) {
+						
+					}
+				};
+				
+				if (current.getField() == null || !(current.getField().getId() > 0))
+					WindowUtils.errorAlert("Please save your changes before adding references.");
+				else
+					SISClientBase.getInstance().onShowReferenceEditor(
+						"Add references to " + getHeading() + " Record #" + current.getId(), 
+						new ReferenceableField(current.getField()), 
+						callback, callback
+					);
+			}
+		});
+
+		optionsMenu.add(referenceMenu);
+		
+		return optionsMenu;
+	}
+	
+	private LayoutContainer createEditingArea() {
+		final ToolBar bar = new ToolBar();
+		bar.add(fullNameDisplay);
+		bar.add(new FillToolItem());
+		bar.add(new IconButton("icon-gear", new SelectionListener<IconButtonEvent>() {
+			public void componentSelected(IconButtonEvent ce) {
+				buildOptionsMenu().show(ce.getIconButton());
+			}
+		}));
+		
+		final ButtonBar bottom = new ButtonBar();
+		bottom.setAlignment(HorizontalAlignment.CENTER);
+		bottom.setSpacing(10);
+		bottom.add(new Button("Done", new SelectionListener<ButtonEvent>() {
+			public void componentSelected(ButtonEvent ce) {
+				stopEditing();
+			}
+		}));
+		
+		final LayoutContainer container = new LayoutContainer(new BorderLayout());
+		container.add(bar, new BorderLayoutData(LayoutRegion.NORTH, 25, 25, 25));
+		container.add(editingArea, new BorderLayoutData(LayoutRegion.CENTER));
+		container.add(bottom, new BorderLayoutData(LayoutRegion.SOUTH, 25, 25, 25));
+		
+		return container;
+	}
+	
+	private BaseModelData newModelData(final StructureHolder structureHolder) {
+		BaseModelData model = new BaseModelData();
+		model.set("model", structureHolder);
+		model.set("id", structureHolder.getId());
+		
+		return model;
+	}
+	
+	private LayoutContainer createListingArea() {
+		final ListViewSelectionModel<BaseModelData> sm = 
+			new ListViewSelectionModel<BaseModelData>();
+		sm.setSelectionMode(SelectionMode.SINGLE);
+		sm.addSelectionChangedListener(new SelectionChangedListener<BaseModelData>() {
+			public void selectionChanged(SelectionChangedEvent<BaseModelData> se) {
+				BaseModelData selection = se.getSelectedItem();
+				if (selection != null)
+					setCurrent((StructureHolder)selection.get("model"));
+			}
+		});
+		
+		view.setSelectionModel(sm);
+		
+		final ButtonBar bar = new ButtonBar();
+		bar.setAlignment(HorizontalAlignment.CENTER);
+		bar.add(new Button("Add", new SelectionListener<ButtonEvent>() {
+			public void componentSelected(ButtonEvent ce) {
+				add();
+			}
+		}));
+		bar.add(new Button("Remove", new SelectionListener<ButtonEvent>() {
+			public void componentSelected(ButtonEvent ce) {
+				final BaseModelData selected = sm.getSelectedItem();
+				if (selected != null) {
+					final StructureHolder holder = selected.get("model");
+					WindowUtils.confirmAlert("Confirm", "Are you sure you want to delete this record?", new WindowUtils.SimpleMessageBoxListener() {
+						public void onYes() {
+							delete(holder, new SimpleListener() {
+								public void handleEvent() {
+									store.remove(selected);
+								}
+							});
+						}
+					});
+				}
+			}
+		}));
+		
+		final LayoutContainer container = new LayoutContainer(new BorderLayout());
+		container.add(view, new BorderLayoutData(LayoutRegion.CENTER));
+		container.add(bar, new BorderLayoutData(LayoutRegion.SOUTH, 25, 25, 25));
+		
+		return container;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void add() {
+		stopEditing();
+				
+		StructureHolder holder = new StructureHolder(DisplayDataProcessor.processDisplayStructure(defaultStructureData));
+				
+		BaseModelData model = newModelData(holder);
+				
+		store.add(model);
+		store.sort("id", SortDir.ASC);
+				
+		setCurrent(holder);
+	}
+	
+	private void delete(final StructureHolder record, final SimpleListener listener) {
+		listener.handleEvent();
+		stopEditing();
+	}
+	
+	public void clear() {
+		store.removeAll();
+	}
+	
+	public DisplayData getDisplayData() {
+		return defaultStructureData;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void setData(Field field) {
+		clear();
+		
+		if (field != null)
+			for (Field subField : field.getFields())
+				store.add(newModelData(new StructureHolder(DisplayDataProcessor.processDisplayStructure(defaultStructureData), subField)));
+		
+		store.sort("id", SortDir.ASC);
+	}
+	
+	public void setEnabled(boolean isEnabled) {
+		for (Iterator<BaseModelData> iter = store.getModels().iterator(); iter.hasNext();) {
+			BaseModelData current = iter.next();
+			StructureHolder model = current.get("model");
+			model.structure.setEnabled(isEnabled);
+		}
+	}
+	
+	public List<StructureHolder> getSelected() {
+		final List<StructureHolder> list = new ArrayList<StructureHolder>();
+		for (BaseModelData data : store.getModels()) {
+			StructureHolder holder = data.get("model");
+			list.add(holder);
+		}
+		return list;
+	}
+
+	public static class StructureHolder {
+		
+		private long id;
+		
+		private Structure<Object> structure;
+		private Field field;
+		
+		public StructureHolder(Structure<Object> structure) {
+			this(structure, null);
+		}
+		
+		public StructureHolder(Structure<Object> structure, Field field) {
+			this.structure = structure;
+			this.field = field;
+			
+			this.structure.setData(field);
+			
+			updateId();		
+		}
+		
+		public long updateId() {
+			this.id = 0;
+			if (field != null)
+				id = field.getId();
+			
+			if (id <= 0)
+				id = new Date().getTime();
+			
+			return id;
+		}
+		
+		public long getId() {
+			return id;
+		}
+		
+		public Field getField() {
+			return field;
+		}
+		
+		public Structure<Object> getStructure() {
+			return structure;
+		}
+		
+		public boolean hasChanged() {
+			//FIXME: ????
+			return structure.hasChanged(field);
+		}
+		
+		public void setField(Field field) {
+			this.field = field;
+		}
+		
+	}
+	
+}
