a91a37f1957f3db27255a043125aea981a473dfb
Carl Scott <carl.scott@solertium.com>
2011-12-29 18:32:22 -0500

    Added assertions, tests to SaveAssessmentTest
    Added assertions to existing test methods as well as
    new test methods for the AssessmentPersistence testing
    class.  Seeing that this could be useful for Offline,
    and is used online as well, it would be good to continue
    to test this and add test cases.
    
    See #645
    
    Change-Id: I01d785e7534f658b7f8ec58b88d4e1f375fa6cd2
    Reviewed-on: http://review.source.gogoego.com/3447
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.server.testing/test/core/SaveAssessmentTest.java
+++ org.iucn.sis.server.testing/test/core/SaveAssessmentTest.java
@@ -1,7 +1,15 @@
 package core;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.Assert;
+
 import org.iucn.sis.server.utils.AssessmentPersistence;
 import org.iucn.sis.shared.api.models.Assessment;
+import org.iucn.sis.shared.api.models.AssessmentChange;
 import org.iucn.sis.shared.api.models.Field;
 import org.iucn.sis.shared.api.models.PrimitiveField;
 import org.iucn.sis.shared.api.models.fields.ThreatsSubfield;
@@ -9,37 +17,121 @@ import org.iucn.sis.shared.api.models.primitivefields.StringPrimitiveField;
 import org.iucn.sis.shared.api.utils.CanonicalNames;
 import org.junit.Test;
 
+import com.solertium.util.events.ComplexListener;
+
 public class SaveAssessmentTest extends BasicTest {
 	
 	@Test
 	public void testSaveNew() throws Exception {
 		Assessment target = new Assessment();
 		
+		Assert.assertTrue(target.getField().isEmpty());
+		
 		AssessmentPersistence saver = new AssessmentPersistence(null, target);
 		saver.sink(createBrandNewAssessmentWithFields());
 		
-		//System.out.println(toXML(target));
+		Assert.assertFalse(target.getField().isEmpty());
+		
+		Field a = target.getField("a");
+		
+		Assert.assertNotNull(a);
+		Assert.assertNotNull(a.getPrimitiveField("value"));
+		Assert.assertEquals("saved value", a.getPrimitiveField("value").getRawValue());
+		
+		List<AssessmentChange> changes = saver.getChangeSet();
+		
+		Assert.assertFalse(changes.isEmpty());
+		Assert.assertEquals("a", changes.get(0).getFieldName());
+		Assert.assertEquals(AssessmentChange.ADD, changes.get(0).getType());
 	}
 	
 	@Test
 	public void testSaveExisting() throws Exception {
+		Assessment source = createExistingAssessmentWithFields();
+		source.getField("a").getPrimitiveField("value").setRawValue("unsaved value");
+		
 		Assessment target = createExistingAssessmentWithFields();
 		
-		AssessmentPersistence saver = new AssessmentPersistence(null, target);
-		saver.sink(createExistingAssessmentWithFields());
-		
-		//System.out.println(toXML(target));
-	}
-	
-	@Test
-	public void testSaveExistingAsDelete() throws Exception {
-		Assessment source = createExistingAssessmentWithFields();
-		Assessment target = createExistingAssessmentWithFields(false);
+		Assert.assertEquals("saved value", target.getField("a").getPrimitiveField("value").getRawValue());
 		
 		AssessmentPersistence saver = new AssessmentPersistence(null, target);
 		saver.sink(source);
 		
-		System.out.println(toXML(target));
+		Assert.assertEquals("unsaved value", target.getField("a").getPrimitiveField("value").getRawValue());
+		
+		List<AssessmentChange> changes = saver.getChangeSet();
+		
+		Assert.assertFalse(changes.isEmpty());
+		Assert.assertEquals(2, changes.size());
+		
+		for (AssessmentChange change : changes)
+			Assert.assertEquals(AssessmentChange.EDIT, change.getType());
+	}
+	
+	@Test
+	public void testSaveExistingAsDeletePrim() throws Exception {
+		Assessment source = createExistingAssessmentWithFields();
+		Assessment target = createExistingAssessmentWithFields();
+		
+		Assert.assertEquals(2, source.getField("b").getPrimitiveField().size());
+		
+		PrimitiveField prim = source.getField("b").getPrimitiveField("value");
+		Assert.assertNotNull(prim);
+		Assert.assertEquals("saved value", prim.getRawValue());
+		
+		source.getField("b").getPrimitiveField().remove(prim);
+		
+		Assert.assertEquals(1, source.getField("b").getPrimitiveField().size());
+		
+		final Map<String, PrimitiveField<?>> deletedPrims = new HashMap<String, PrimitiveField<?>>();
+		
+		AssessmentPersistence saver = new AssessmentPersistence(null, target);
+		saver.setDeletePrimitiveFieldListener(new ComplexListener<PrimitiveField<?>>() {
+			public void handleEvent(PrimitiveField<?> eventData) {
+				deletedPrims.put(eventData.getName(), eventData);
+			}
+		});
+		saver.sink(source);
+		
+		//System.out.println(toXML(target));
+		
+		Assert.assertFalse(deletedPrims.isEmpty());
+		Assert.assertNotNull(deletedPrims.get("value"));
+		
+		Assert.assertNotNull(target.getField("b").getPrimitiveField("another"));
+		
+		Assert.assertEquals(2, saver.getChangeSet().size());
+		for (AssessmentChange change : saver.getChangeSet())
+			Assert.assertEquals(AssessmentChange.EDIT, change.getType());
+	}
+	
+	@Test
+	public void testSaveExistingAsDeleteField() throws Exception {
+		Assessment source = createExistingAssessmentWithFields();
+		Assessment target = createExistingAssessmentWithFields();
+		
+		Assert.assertEquals(2, source.getField().size());
+		
+		source.getField().remove(source.getField("b"));
+		
+		Assert.assertEquals(1, source.getField().size());
+		
+		final Map<String, Field> deletedFields = new HashMap<String, Field>();
+		
+		AssessmentPersistence saver = new AssessmentPersistence(null, target);
+		saver.setDeleteFieldListener(new ComplexListener<Field>() {
+			public void handleEvent(Field eventData) {
+				deletedFields.put(eventData.getName(), eventData);
+			}
+		});
+		saver.sink(source);
+		
+		//System.out.println(toXML(target));
+		
+		Assert.assertFalse(deletedFields.isEmpty());
+		Assert.assertNotNull(deletedFields.get("b"));
+		
+		Assert.assertNotNull(target.getField("a"));
 	}
 	
 	@Test
@@ -96,7 +188,7 @@ public class SaveAssessmentTest extends BasicTest {
 	}
 	
 	private Assessment createExistingAssessmentWithFields() {
-		return createExistingAssessmentWithFields(false);
+		return createExistingAssessmentWithFields(true);
 	}
 	
 	private Assessment createExistingAssessmentWithFields(boolean fieldB) {
@@ -117,7 +209,7 @@ public class SaveAssessmentTest extends BasicTest {
 			assessment.getField().add(a);
 		}
 		
-		{ 
+		if (fieldB) { 
 			Field b = new Field("b", assessment);
 			b.setId(2);
 			b.getPrimitiveField().add(new StringPrimitiveField("value", b, "saved value"));
