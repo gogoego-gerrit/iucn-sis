9f10c39cd58e8ab0ac6a4c3353c3d06e8276b75a
Carl Scott <carl.scott@solertium.com>
2012-01-30 17:56:21 -0500

    OnLoad, ensure appropriate offline data available.
    When initializing and loading data (offline metadata and
    working set), to the client, ensure that the metadata and
    the working set are properly loaded.  If either are not
    loaded (this can happen when a user without right to the
    offline working set attempts to login), prompt the user
    with a message stating the error.
    
    See #790
    
    Change-Id: I37849b87f0e1817ece9bdf574ce38ea9c2518e24
    Reviewed-on: http://review.source.gogoego.com/3718
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.client/src/org/iucn/sis/client/container/OfflineSimpleSISClient.java
+++ org.iucn.sis.client/src/org/iucn/sis/client/container/OfflineSimpleSISClient.java
@@ -5,10 +5,12 @@ import java.util.Collection;
 import java.util.List;
 
 import org.iucn.sis.client.api.caches.OfflineCache;
+import org.iucn.sis.client.api.caches.WorkingSetCache;
 import org.iucn.sis.client.api.container.SISClientBase;
 import org.iucn.sis.client.api.utils.HasCache;
 
 import com.google.gwt.user.client.Window;
+import com.solertium.util.extjs.client.WindowUtils;
 
 public class OfflineSimpleSISClient extends SimpleSISClient {
 	
@@ -26,6 +28,16 @@ public class OfflineSimpleSISClient extends SimpleSISClient {
 		List<HasCache> list = new ArrayList<HasCache>(super.getCachesToInitialize());
 		list.add(OfflineCache.impl);		
 		return list;
+	}
+	
+	@Override
+	public void buildPostLogin() {
+		if (OfflineCache.impl.get() == null)
+			WindowUtils.errorAlert("Could not load working set metadata, please check your installation parameters.");
+		else if (WorkingSetCache.impl.getOfflineWorkingSet() == null)
+			WindowUtils.errorAlert("Sorry, you do not have permission to edit the offline working set. Ask the working set owner to share it with you.");
+		else
+			super.buildPostLogin();
 	}
 	
 }
