bd674f89548c2c56eeac53348b4d0de09b5138c9
Carl Scott <carl.scott@solertium.com>
2012-03-19 11:42:04 -0400

    Library addition to the test file getter class.
    Pull a file as a NativeDocument.
    
    Change-Id: I833f9c46b38dbfdb1183d31a22481db904aa17a8
    Reviewed-on: http://review.source.gogoego.com/3908
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- org.iucn.sis.server.testing/test/files/Files.java
+++ org.iucn.sis.server.testing/test/files/Files.java
@@ -1,12 +1,34 @@
 package files;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.w3c.dom.Document;
 
+import com.solertium.lwxml.java.JavaNativeDocument;
+import com.solertium.lwxml.shared.NativeDocument;
 import com.solertium.util.BaseDocumentUtils;
 
 public class Files {
+	
+	public static NativeDocument getNativeDocument(String name) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(get(name)));
+		StringBuilder in = new StringBuilder();
+		String line = null;
+		
+		try {
+			while ((line = reader.readLine()) != null)
+				in.append(line);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		NativeDocument document = new JavaNativeDocument();
+		document.parse(in.toString());
+		
+		return document;
+	}
 	
 	public static Document getXML(String name) {
 		return BaseDocumentUtils.impl.getInputStreamFile(get(name));
