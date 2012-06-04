9f6885e99dfa001eda171cf4f0eee10be37d87c4
Carl Scott <carl.scott@solertium.com>
2012-06-04 15:13:38 -0400

    Correction/improvement to vw_published filter.
    A couple of items changed here to make this work a
    little better...
    
    * Split off inner query into recent_by_region table.
    * Ensure recent_regional ignores deleted assessments.
    * Ignore assessments without critVersion 2.3 or 3.1
    
    See #946
    
    Change-Id: I8c6f599e7bd8fed2ee56312d7cdc50b591ae1377
    Reviewed-on: http://review.source.gogoego.com/4266
    Reviewed-by: Carl Scott <carl.scott@solertium.com>
    Tested-by: Carl Scott <carl.scott@solertium.com>

--- viewmaster/org.iucn.sis.viewmaster/src/org/iucn/sis/viewmaster/RedListPublish3.sql
+++ viewmaster/org.iucn.sis.viewmaster/src/org/iucn/sis/viewmaster/RedListPublish3.sql
@@ -1,6 +1,23 @@
+DROP TABLE IF EXISTS vw_published.recent_by_region;
+CREATE TABLE vw_published.recent_by_region AS
+  SELECT taxonid, fi2.value as region, MAX(df1.value) as date
+  FROM assessment a
+  JOIN taxon ON a.taxonid = taxon.id
+  LEFT JOIN field f1 ON f1.assessmentid = a.id AND f1.name = 'RedListAssessmentDate'
+  LEFT JOIN primitive_field pf1 ON f1.id = pf1.fieldid AND pf1.name = 'value'
+  LEFT JOIN date_primitive_field df1 ON pf1.id = df1.id AND df1.value >= '1996-01-01'
+  LEFT JOIN field f2 ON f2.assessmentid = a.id AND f2.name = 'RegionInformation'
+  LEFT JOIN primitive_field pf2 ON f2.id = pf2.fieldid AND pf2.name = 'regions'
+  LEFT JOIN foreign_key_list_primitive_field fk2 ON pf2.id = fk2.id
+  LEFT JOIN fk_list_primitive_values fi2 ON fk2.id = fi2.fk_list_primitive_id
+  WHERE taxon.taxon_statusid NOT IN (2, 3) AND
+    a.state = 0 AND
+    a.assessment_typeid = 1
+  GROUP BY taxonid, fi2.value;
+
 DROP TABLE IF EXISTS vw_published.recent_regional;
 CREATE TABLE vw_published.recent_regional AS
-  SELECT a.id AS assessmentid
+  SELECT a.id AS assessmentid, a.taxonid
   FROM assessment a
   JOIN field f1 ON f1.assessmentid = a.id AND f1.name = 'RedListAssessmentDate'
   JOIN primitive_field pf1 ON f1.id = pf1.fieldid AND pf1.name = 'value'
@@ -9,39 +26,27 @@ CREATE TABLE vw_published.recent_regional AS
   JOIN primitive_field pf2 ON f2.id = pf2.fieldid AND pf2.name = 'regions'
   JOIN foreign_key_list_primitive_field fk2 ON pf2.id = fk2.id
   JOIN fk_list_primitive_values fi2 ON fk2.id = fi2.fk_list_primitive_id
-  WHERE a.assessment_typeid = 1 AND (a.taxonid, fi2.value, df1.value) IN (
-    SELECT taxonid, fi2.value as region, MAX(df1.value) as date
-    FROM assessment a
-    JOIN taxon ON a.taxonid = taxon.id
-    LEFT JOIN field f1 ON f1.assessmentid = a.id AND f1.name = 'RedListAssessmentDate'
-    LEFT JOIN primitive_field pf1 ON f1.id = pf1.fieldid AND pf1.name = 'value'
-    LEFT JOIN date_primitive_field df1 ON pf1.id = df1.id AND df1.value >= '1996-01-01'
-    LEFT JOIN field f2 ON f2.assessmentid = a.id AND f2.name = 'RegionInformation'
-    LEFT JOIN primitive_field pf2 ON f2.id = pf2.fieldid AND pf2.name = 'regions'
-    LEFT JOIN foreign_key_list_primitive_field fk2 ON pf2.id = fk2.id
-    LEFT JOIN fk_list_primitive_values fi2 ON fk2.id = fi2.fk_list_primitive_id
-    WHERE taxon.taxon_statusid NOT IN (2, 3) AND
-      a.state = 0 AND
-      a.assessment_typeid = 1
-    GROUP BY taxonid, fi2.value
-  );
+  JOIN vw_published.recent_by_region rbr ON a.taxonid = rbr.taxonid AND
+    fi2.value = rbr.region AND df1.value = rbr.date
+  WHERE a.state = 0 AND a.assessment_typeid = 1;
   
 TRUNCATE vw_published.vw_filter;
 INSERT INTO vw_published.vw_filter (assessmentid, taxonid)
-  SELECT DISTINCT a.id as assessmentid, a.taxonid
-  FROM assessment a
-  JOIN vw_published.recent_regional rr ON a.id = rr.assessmentid
-  JOIN field f ON f.assessmentid = a.id AND f.name = 'RedListCriteria'
+  SELECT DISTINCT a.assessmentid, a.taxonid
+  FROM vw_published.recent_regional a
+  JOIN field f ON f.assessmentid = a.assessmentid AND f.name = 'RedListCriteria'
   LEFT JOIN primitive_field pf1 ON pf1.fieldid = f.id AND pf1.name = 'isManual'
   LEFT JOIN boolean_primitive_field bf1 ON pf1.id = bf1.id
   LEFT JOIN primitive_field pf2 ON pf2.fieldid = f.id AND pf2.name = 'autoCategory'
   LEFT JOIN string_primitive_field sf2 ON sf2.id = pf2.id
   LEFT JOIN primitive_field pf3 ON pf3.fieldid = f.id AND pf3.name = 'manualCategory'
   LEFT JOIN string_primitive_field sf3 ON sf3.id = pf3.id
-  LEFT JOIN field f2 ON f2.assessmentid = a.id AND f2.name = 'RedListHidden'
+  LEFT JOIN primitive_field pf5 ON pf5.fieldid = f.id AND pf5.name = 'critVersion'
+  LEFT JOIN foreign_key_primitive_field fk5 ON fk5.id = pf5.id
+  LEFT JOIN field f2 ON f2.assessmentid = a.assessmentid AND f2.name = 'RedListHidden'
   LEFT JOIN primitive_field pf4 ON pf4.fieldid = f2.id AND pf4.name = 'value'
   LEFT JOIN boolean_primitive_field bf4 ON bf4.id = pf4.id
-  WHERE bf4.value is null AND 
+  WHERE bf4.value is null AND fk5.value < 3 AND
     CASE
       WHEN bf1.value = true THEN sf3 is not null AND sf3.value <> 'NE'
       ELSE sf2 is not null AND sf2.value <> 'NE'
